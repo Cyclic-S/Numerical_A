b66fd2eea21572ed3ca3edc6631b435c2bfb6e72
int <INTmath@outlook.com>
2023-10-31 12:20:06 +0800

    更新

---
+++ w9/work9q1.m
@@ -0,0 +1,18 @@
+clc
+clear
+syms x
+f=1/(1+25*x^2);
+N=[3 6 10];
+S=[x x x];
+for i=1:3
+    s=0;
+    for j=0:N(i)
+        s=s+int(legendremap(j)*f,-1,1)*legendremap(j)*(2*j+1)/2;%自定义函数legendremap第七周作业
+    end
+    S(i)=collect(s,x);
+    fplot(S(i),[-1,1]);
+    hold on
+end
+hold on
+fplot(f,[-1 1]);
+legend({'$S_3^*$','$S_6^*$','$S_{10}^*$','$f(x)$'},'Interpreter','latex');
---
+++ w9/work9q2.m
@@ -0,0 +1,86 @@
+%% base {1,x,x^2}
+clc
+clear
+G=zeros(3,3);
+syms x
+B=[1 x x^2];
+X=[-1 -0.8 -0.6 -0.4 -0.3 -0.1 0 0.2 0.4 0.6 0.8 1];
+Y=[0 1.27 2.16 2.86 3.44 3.87 4.15 4.37 4.51 4.58 4.62 4.64];
+%Gram Matrix
+for i=2:3
+    f1=matlabFunction(B(i));
+    for j=2:3
+        f2=matlabFunction(B(j));
+        G(i,j)=f1(X)*f2(X)';
+    end
+end
+for j=2:3
+    f3=matlabFunction(B(j));
+    G(1,j)=sum(f3(X));
+end 
+G(1,1)=length(X);
+for j=2:3
+    G(j,1)=G(1,j);
+end
+D=zeros(3,1);
+for i=1:3
+    if i==1
+        D(i,1)=sum(Y);
+    else
+        f4=matlabFunction(B(i));
+        D(i,1)=f4(X)*Y';
+    end
+end
+A=G\D;
+y=0;
+for i=1:length(A)
+    y=y+A(i)*x^(i-1);
+end
+y=collect(y,x);
+fplot(y,[min(X) max(X)]);
+hold on
+plot(X,Y,'ro');
+legend({'二次拟合多项式曲线','实验数据'});
+%% base {1,x,x^2,x^3}
+clc
+clear
+G=zeros(4,4);
+syms x
+B=[1 x x^2 x^3];
+X=[-1 -0.8 -0.6 -0.4 -0.3 -0.1 0 0.2 0.4 0.6 0.8 1];
+Y=[0 1.27 2.16 2.86 3.44 3.87 4.15 4.37 4.51 4.58 4.62 4.64];
+%Gram Matrix
+for i=2:4
+    f1=matlabFunction(B(i));
+    for j=2:4
+        f2=matlabFunction(B(j));
+        G(i,j)=f1(X)*f2(X)';
+    end
+end
+for j=2:4
+    f3=matlabFunction(B(j));
+    G(1,j)=sum(f3(X));
+end 
+G(1,1)=length(X);
+for j=2:4
+    G(j,1)=G(1,j);
+end
+D=zeros(4,1);
+for i=1:4
+    if i==1
+        D(i,1)=sum(Y);
+    else
+        f4=matlabFunction(B(i));
+        D(i,1)=f4(X)*Y';
+    end
+end
+A=G\D;
+y=0;
+for i=1:length(A)
+    y=y+A(i)*x^(i-1);
+end
+y=collect(y,x);
+fplot(y,[min(X) max(X)]);
+hold on
+plot(X,Y,'ro');
+legend({'二次拟合多项式曲线','实验数据'});
