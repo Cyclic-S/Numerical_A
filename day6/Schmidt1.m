0b0432438e58d50669c29199d02244fe182467cb
int <INTmath@outlook.com>
2023-11-26 12:56:49 +0800

    更新

--- day6/Schmidt1.m
+++ day6/Schmidt1.m
@@ -1,14 +1,17 @@
 function s=Schmidt1(a,b,rho,n)
+%Schmidt正交化方法
 syms x
-sum=0;
 P=[1,x];
 for i=2:n
     P=[P,x^i];
 end
 for i=2:n
+    sum=0;
     for k=1:i
-        sum=sum-P(k)*int(rho*x^i*P(k),a,b)/int(rho*P(k)*P(k),a,b)*P(k);
+        sum=sum+P(k)*int(rho*x^i*P(k),a,b)/int(rho*P(k)*P(k),a,b);
     end
-    P(i+1)=x^i+sum;
+    P(i+1)=x^i-sum;
 end
 s=P;
+%示例Legendre多项式
+%Schmidt1(-1,1,1,6)
--- day6/legendremap.m
+++ day6/legendremap.m
@@ -1,5 +1,5 @@
 function y=legendremap(n)
-%递推公式法
+%递推公式法求Legendre多项式
 syms x;
 P0=1;
 P1=x;
@@ -17,5 +17,6 @@ elseif n==1
 else
     y=collect(P_iadd1);
 end
-
 end
+%示例
+%legendremap(6)
