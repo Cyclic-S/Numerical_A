ccddc4903924771bfacd5e89c088f52cb246008a
int <INTmath@outlook.com>
2023-12-18 15:55:58 +0800

    更新

--- w13/lu_decomposition.m
+++ w13/lu_decomposition.m
@@ -1,19 +1,4 @@
 function [L,U]=lu_decomposition(A)
-% n=length(A);
-% L=eye(n)+zeros(n,n);
-% U=[A(1,:);zeros(n-1,n)];
-% %L(1,:)
-% for i=1:n
-%     L(i,1)=A(i,1)/A(1,1);
-% end
-% %U(:,2)
-% for j=2:n
-%     U(2,j)=A(2,j)-L(2,1)*A(1,j);
-% end
-% %L(2,:)
-% for i=1:n
-%     L(i,2)=
-% end
 [m,n]=size(A);
 L=eye(m);
 L(:,1)=A(:,1)/A(1,1);%L第一列赋值
